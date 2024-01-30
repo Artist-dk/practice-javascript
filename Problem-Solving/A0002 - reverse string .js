ea929fd2b0501b80811758996bd669cf1cb32e78
Digambar Kumbhar <126682017+Artist-dk@users.noreply.github.com>
2024-01-30 19:01:12 +0530

    Create A0002 - reverse string .js

---
+++ Problem-Solving/A0002 - reverse string .js
@@ -0,0 +1,13 @@
+// Reverse a String
+
+
+var str = "Hello"
+var arr = str.split("")
+arr = arr.reverse()
+arr = arr.toString()
+arr = arr.replaceAll(",","")
+console.log(arr)
+
+
+// i/p = "Hello"
+// o/p = "olleH"
