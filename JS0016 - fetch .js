541e41d7b1565dacb66c3086ef9f9c5ae7504c29
Artist-dk <digambarckumbhar299@gmail.com>
2024-02-04 16:15:17 +0530

    modified: JS0016 - fetch .js

--- JS0016 - fetch .js
+++ JS0016 - fetch .js
@@ -1,7 +1,10 @@
-const url = "https://jgentes-crime-data-v1.p.rapidapi.com/crime"
+const url = "https://api.publicapis.org/entries"
 const getFacts = async () => {
   console.log("getting data...")
   let response = await fetch(url)
   console.log(response.body)
+  let data = await response.json();
+  console.log(data)
+
 }
 getFacts();
