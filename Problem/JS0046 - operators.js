89bd720dbfe4622a612996cd12d52eea9bd0f94b
Digambar Kumbhar <digambarckumbhar299@gmail.com>
2024-12-03 03:02:35 +0530

    Added new practice programs

---
+++ Problem/JS0046 - operators.js
@@ -0,0 +1,24 @@
+// Operators
+
+/* Arithmetic Operators
+1. + -> Addition
+2. - -> Subtraction
+3. * -> Multiplication
+4. / -> Division
+5. % -> Modulus
+6. ++ -> Increment
+7. -- -> Decrement 
+*/
+
+var a = 23
+var b = 10
+
+var c = "Test"
+
+console.log(`L1 : a + b = ${a+b}`)
+console.log(`L2 : a - b = ${a-b}`)
+console.log(`L3 : a * b = ${a*b}`)
+console.log(`L4 : a / b = ${a/b}`)
+console.log(`L5 : a % b = ${a%b}`)
+console.log(`L6 : ++a = ${++a}`)
+console.log(`L7 : --a = ${--a}`)
---
+++ Problem/JS0049 - operators.js
@@ -0,0 +1,18 @@
+/* Assignment Operators
+1. = -> Simple assignment
+2. += -> Add and assignment
+3. -+ -> Subtract and assignment
+4. *= -> Multiply and assignemnt
+5. /= -> Divide and assignment
+6. %= -> Modules and assignment
+*/
+
+var a = 33;
+var b = 10;
+
+console.log(`L1 : a = b -> ${a = b}`)
+console.log(`L2 : a += b -> ${a += b}`)
+console.log(`L3 : a -= b -> ${a -= b}`)
+console.log(`L4 : a *= b -> ${a *= b}`)
+console.log(`L5 : a /= b -> ${a /= b}`)
+console.log(`L6 : a %= b -> ${a %= b}`)
