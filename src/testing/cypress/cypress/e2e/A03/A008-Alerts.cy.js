c688063690012e98b48f0247aaa212670e5fbe0a
digambar kumbhar <digambarckumbhar299@gmail.com>
2025-03-03 21:56:51 +0530

    Practiced: Alert tests in cypress

--- src/testing/cypress/cypress/e2e/A03/A008-Alerts.cy.js
+++ src/testing/cypress/cypress/e2e/A03/A008-Alerts.cy.js
@@ -17,13 +17,52 @@ describe('Alerts',()=>{
     cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
     cy.get('button[onclick="jsConfirm()"]').click();
     cy.on('window:confirm',(t)=>{
-      expect(t).to.contains('I am JS confirm');
+      expect(t).to.contains('I am a JS Confirm');
     })
     cy.get('#result').should('have.text','You clicked: Ok')
   })
+  it('Js confirm alert - cancel',()=>{
+    cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
+    cy.get('button[onclick="jsConfirm()"]').click();
+    cy.on('window:confirm',()=>false)
+    cy.get('#result').should('have.text','You clicked: Cancel')
+  })
   // 3) Javascript Prompt Alert: It will have some text with a text box for user input along with 'OK'
 
+  
+  it('Js prompt alert',()=>{
+    cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
+    cy.window().then((win)=>{
+      cy.stub(win,'prompt').returns('welcome');
+    })
+    cy.get('button[onclick="jsPrompt()"]').click();
+    cy.get("#result").should('have.text','You entered: welcome')
+  })
+  
+  // it.only('Js prompt alert - cancel',()=>{
+  //   cy.visit('http://the-internet.herokuapp.com/javascript_alerts')
+  //   cy.window().then((win)=>{
+  //     cy.stub(win,'prompt').returns('welcome');
+  //   })
+  //   cy.get('button[onclick="jsPrompt()"]').click();
+  //   cy.on('window:prompt',()=>false)
+  //   cy.get("#result").should('have.text','You entered: null')
+  // })
+
   // 4) Authenticated Alert
+
+  // approach 1
+  it('Authenticated alert',()=>{
+    cy.visit('https://the-internet.herokuapp.com/basic_auth',{auth: {username: "admin", password: "admin"}})
+    cy.get('.example p').should('have.contain','Congratulations!')
+  })
+
+  // approach 2
+  it.only('Authenticated alert',()=>{
+    cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
+    cy.get('div.example p').should('have.contain','Congratulations!')
+  })
+
 })
 
 // https://the-internet.herokuapp.com/javascript_alerts
